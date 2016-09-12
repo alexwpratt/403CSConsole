f56382e00881341868e4d42dada861aaaa8be71f
Alex Wilcken Pratt <alex.w.pratt@gmail.com>
2016-09-12 14:44:22 -0600

    create Team.cs

---
+++ Team.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSConsoleAssignment
+{
+    class Team
+    {
+        //class attributes:
+        public string name;
+        public int wins;
+        public int loss;
+
+        //Constructors:
+
+        //empty contsructor
+        public Team(){}
+
+    }
+}
